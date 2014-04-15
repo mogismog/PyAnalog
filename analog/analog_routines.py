031b211ddacc1a53c56402dd9c84635b65925e59
Francisco Alvarez <falvare6@slu.edu>
2014-04-14 21:36:47 -0500

    Updated Analog class, added in configuration and domain setup for analogs.

--- analog/analog_routines.py
+++ analog/analog_routines.py
@@ -40,10 +40,9 @@ class Analog(object):
             if n_vars > 1, can use different methods to compare each variable.
         """
 
-        # --- Make sure things are copacetic prior to setting things...
-
         # --- Here, we check to make sure everything is copacetic.
         if (len(self.forecast.shape) > 2) and (self.forecast.shape[0] > 1):
+            self.n_vars = self.forecast.shape[0]
             # --- First, make sure we're dealing with same number of vars between fcst/train
             if self.forecast.shape[0] != self.train.shape[0]:
                 raise ValueError("Different number of variables between forecast and training data arrays!")
@@ -52,6 +51,8 @@ class Analog(object):
                 raise ValueError("More weights than variables!")
             if len(comp_method) > self.forecast.shape[0]:
                 raise ValueError("More methods than variables!")
+        else:
+            self.n_vars = 1
 
         # --- Everything cool? Ok, let's do this.
         self.window = window
@@ -62,7 +63,7 @@ class Analog(object):
 
     def domain(self, lat_bounds=[], lon_bounds=[], forecast_lats=[], forecast_lons=[], lat_inc=1, lon_inc=1):
         """
-        Domain setup.
+        Main and forecast domain setup.
 
         Parameters
         ----------
@@ -73,96 +74,41 @@ class Analog(object):
             Either a list/numpy array of min/max forecast lats/lons (defining a domain) or a single point.
 
 
-        comp_method : list
-            Method with which to pattern match. Options: ['Rank','MAE','RMSE','Corr'].
-            if n_vars > 1, can use different methods to compare each variable.
+        lat_inc/lon_inc : float
+            The change in latitude/longitude each grid point (e.g. dx or dy).
         """
-
-        # --- Make sure things are copacetic prior to setting things...
-
         # --- Here, we check to make sure everything is copacetic.
-        if (len(self.forecast.shape) > 2) and (self.forecast.shape[0] > 1):
-            # --- First, make sure we're dealing with same number of vars between fcst/train
-            if self.forecast.shape[0] != self.train.shape[0]:
-                raise ValueError("Different number of variables between forecast and training data arrays!")
-            # --- Make sure there aren't more weights than variables
-            if len(weights) > self.forecast.shape[0]:
-                raise ValueError("More weights than variables!")
-            if len(comp_method) > self.forecast.shape[0]:
-                raise ValueError("More methods than variables!")
+        if len(forecast_lats) != len(forecast_lons):
+            raise ValueError("len(lat_bounds) != len(lon_bounds)\nWe either need to forecast for a point or domain.")
 
         # --- Everything cool? Ok, let's do this.
-        self.window = window
-        self.comp_method = comp_method
-        self.weights = weights
+        if len(forecast_lats) == 1:
+            self.point_fcst = True
+        else:
+            self.point_fcst = False
+
+        self.allLats = np.arange(lat_bounds[0],lat_bounds[-1]+1,lat_inc)
+        self.allLons = np.arange(lon_bounds[0],lon_bounds[-1]+1,lon_inc)
+        self.fcstLats = forecast_lats
+        self.fcstLons = forecast_lons
 
         return self
 
 
-    def compare(fcst, train, latidxs, lonidxs, weight, method):
+    def analog_single_date(self):
         """
-
-
-        :param weight:
-        :param method:
-        :param fcst:
-        :param train:
-        :param latidxs:
-        :param lonidxs:
-        """
-        return fcst
-
-    def analog_single_date(forecastData, trainingData, forecastLats, forecastLons, allLats, allLons, \
-                           window, **kwargs):
-        """
-        Method to pattern match and find analogous dates at either single location or multiple grid points.
-
-        Used for a single forecast date.
-
-
-        forecastData - Either a 2-d (lat,lon) or 3-d (n_vars,lat,lon) numpy array of "current" forecast data.
-        trainingData - Either a 3-d (time,lat,lon) or 4-d (n_vars,time,lat,lon) numpy array of "past"/training data.
-        forecastLats - Either a list/numpy array of min/max forecast lats (defining a domain) or a single point.
-        forecastLons - Either a list/numpy array of min/max forecast lons (defining a domain) or a single point.
-        allLats - A list/numpy array of all latitudes within the forecast and training data.\n
-            len(allLats) = forecastData[0] or forecastData[1] if forecastData.shape == 3.
-        allLons - A list/numpy array of all longitudes within the forecast and training data.\n
-            len(allLons) = forecastData[0] or forecastData[1] if forecastData.shape == 3.
-        window - +/- number of grid points (n/s, e/w) around each forecast grid point to include in local domain pattern\n
-            matching. For example, if window = 3, we use 49 grid points to calculate differences between forecast/training\n
-            data (3*2 + 1)*(3+2 +1). If one_dom = True, this is ignored.
-
-
-        optional:
-            method - Method with which to pattern match. Options: ['Rank','MAE','RMSE','Corr'].
-                if n_vars > 1, can use different methods to compare each variable.
-            one_dom - If we're forecasting for multiple grid points, this can set it to pattern match one big domain.
-            weights - If interested in an additive model, weights can be set based on the importance of each item.
-                note: sum(weights) == 1.
-
+        Used to find analogs for a single forecast domain.
 
         returns:
             analog_idxs - indices of closest analogs, from best pattern match to worst.
         """
-        meths = kwargs.get('method', ['Rank'])
-        one_dom = kwargs.get('one_dom', False)
-        weights = kwargs.get('weights', [])
-        n_proc = kwargs.get('n_proc', 1)
 
-        # --- Here, we check to make sure everything is copacetic.
-        if (forecastData.shape > 2) and (forecastData.shape[0] > 1):
-            # --- First, make sure we're dealing with same number of vars between fcst/train
-            if forecastData.shape[0] != trainingData.shape[0]:
-                raise ValueError("Different number of variables between forecast and training data arrays!")
-            # --- Make sure there aren't more weights than variables
-            if len(weights) > forecastData.shape[0]:
-                raise ValueError("More weights than variables, exiting...")
-            if len(meths) > forecastData.shape[0]:
-                raise ValueError("More methods than variables, exiting...")
+        # --- Pre-generating analog indices array, this should be faster.
+        if len(self.fcstLats) <= 1:
+            analog_idxs = np.ones(self.train.shape[0])*-9999.9
+        else:
+            analog_idxs = np.ones(self.train.shape[0])*-9999.9
 
-
-        # --- Analog method if we're pattern matching one large domain
-        #if one_dom:
 
 
 
