7ffefe447a564d90d712334a56fb26e3fb9a8a7b
Francisco Alvarez <falvare6@slu.edu>
2014-04-15 15:32:33 -0500

    Adding fortran subroutines file.

--- analog/analog_routines.py
+++ analog/analog_routines.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import numpy as np
-from scipy.stats import rankdata as rd
 from ..utils import find_nearest_idx
 
 
@@ -89,37 +88,17 @@ class Analog(object):
 
         self.allLats = np.arange(lat_bounds[0], lat_bounds[-1] + 1, lat_inc)
         self.allLons = np.arange(lon_bounds[0], lon_bounds[-1] + 1, lon_inc)
-        self.fcstLats = forecast_lats
-        self.fcstLons = forecast_lons
+        # --- If we're dealing with a point forecast, we will find the closest lat/lon grid points to the fcst point.
+        if len(forecast_lats) > 1:
+            self.closest_lat = self.allLats[find_nearest_idx(self.allLats[:], self.fcstLats[0])[0]]
+            self.closest_lon = self.allLons[find_nearest_idx(self.allLons[:], self.fcstLons[0])[0]]
+        else:
+            self.fcstLats = forecast_lats
+            self.fcstLons = forecast_lons
 
         return self
 
-    def compare(self, fcst, train, idxs):
-        """
-        Does the heavy lifting in finding analogs.
-
-        Parameters
-        ----------
-        self : object
-            Analog object.
-        fcst : NumPy array
-            Local domain forecast data, comparing this to past forecasts.
-        train : NumPy array
-            Local domain training data, "past" forecasts, reanalysis, whatever.
-        idxs : NumPy array
-            Empty array, used for indices for closest matching patters.
-
-        Returns
-        -------
-        idxs : NumPy array
-            Array of indices for closest matching patters.
-        """
-
-        if self.n_vars == 1:
-            if self.comp_method == "Rank":
-                temp_ranks = np.zeros((self.train[0]+1,self.train[0,...].reshape(-1).shape[0]))
-
-    def analog_single_date(self):
+    def closest_dates_1fcst(self):
         """
         Used to find analogs for a single forecast domain.
 
@@ -136,16 +115,6 @@ class Analog(object):
 
         # --- Ok, let's do this...
         if self.point_fcst:
-            # --- We only need one local analog for this, so find closest lat/lon grid point and form from there.
-            nearest_lat_idx = find_nearest_idx(self.allLats[:], self.fcstLats[0])[0]
-            nearest_lon_idx = find_nearest_idx(self.allLons[:], self.fcstLons[0])[0]
-
-            # --- We then form a domain around the point in question...
-            temp_forecast = self.forecast[int(nearest_lat_idx - self.window):int(nearest_lat_idx - self.window) + 1, \
-                            int(nearest_lon_idx - self.window):int(nearest_lon_idx - self.window) + 1]
-
-            temp_train = self.train[..., int(nearest_lat_idx - self.window):int(nearest_lat_idx - self.window) + 1, \
-                         int(nearest_lon_idx - self.window):int(nearest_lon_idx - self.window) + 1]
 
             # --- We then find the closest analogs
             idxs = compare(self,temp_forecast,temp_train)
