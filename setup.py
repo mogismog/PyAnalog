997942e9370125b39396c5027a7e62d8b90eb1f2
Francisco Alvarez <falvare6@slu.edu>
2014-04-16 11:27:37 -0500

    Reorganized stuff, yay.

--- setup.py
+++ setup.py
@@ -1,12 +1,40 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2013-2014 Francisco Alvarez <francisco.m.alvarez@gmail.com>
+
+DESCRIPTION = "PyAnalog - Weather forecasting with analogous past dates."
+DISTNAME = 'pyanalog'
+MAINTAINER = 'Francisco M. Alvarez'
+MAINTAINER_EMAIL = 'francisco.m.alvarez@gmail.com'
+URL = 'https://github.com/mogismog/PyAnalog'
+LICENSE = 'Apache v2 License'
+DOWNLOAD_URL = 'https://github.com/mogismog/PyAnalog'
+VERSION = '0.1 dev'
+
+
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
+
     config = Configuration('analog', parent_package, top_path)
     config.add_extension('fortran_analog',
-                         sources=['./analog/fortran_routines.f90','./analog/fortran_analog.pyf',
-                                  ])
+                         sources=['./analog/fortran_routines.f90', './analog/fortran_analog.pyf',
+                         ])
     return config
 
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+
+    setup(maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          packages=['pyanalog', 'pyanalog.analog', 'pyanalog.fortran_routines', \
+                    'pyanalog.get_dates', 'pyanalog.utils'],
+          author='Francisco Alvarez',
+          author_email='francisco.m.alvarez@gmail.com',
+          install_requires=["numpy"],
+          **configuration(top_path='').todict())
